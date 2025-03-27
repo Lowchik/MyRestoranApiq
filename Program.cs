480432cfbdb6b4f60c5b98cf7377b7233070ff10
HomeLowchik <offnikita.nik0090@gmail.com>
2025-03-27 14:57:43 +0300

    dasa

--- Program.cs
+++ Program.cs
@@ -10,33 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Добавление поддержки контроллеров
 builder.Services.AddControllers();
 
-// Добавление CORS (разрешаем все домены, методы и заголовки)
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAllOrigins", builder =>
-    {
-        builder.AllowAnyOrigin()    // Разрешаем все источники
-               .AllowAnyMethod()    // Разрешаем любые методы (GET, POST и т.д.)
-               .AllowAnyHeader();   // Разрешаем любые заголовки
-    });
-});
-
-// Добавление Swagger для API тестирования
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-// Включение Swagger UI в режиме разработки
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-
-// Включение CORS
-app.UseCors("AllowAllOrigins");  // Это важно для разрешения CORS
 
 // Маршрут для контроллеров API
 app.MapControllers();
