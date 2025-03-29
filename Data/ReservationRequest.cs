96caa446727f96a66560a5fe61c294cf686ca176
HomeLowchik <offnikita.nik0090@gmail.com>
2025-03-29 12:49:51 +0300

    dsad

--- Data/ReservationRequest.cs
+++ Data/ReservationRequest.cs
@@ -6,10 +6,9 @@ public class Reservation
     public int Id { get; set; }
     public int CustomerId { get; set; }
     public int TableId { get; set; }
-    public DateTime ReservationTime { get; set; }
+    public DateTimeOffset ReservationTime { get; set; } // Используем DateTimeOffset для учета временной зоны
     public int EmployeeId { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow; // Убедитесь, что время в UTC
     public string Status { get; set; } = "Reserved"; // Всегда "Reserved"
-
-   
 }
+
