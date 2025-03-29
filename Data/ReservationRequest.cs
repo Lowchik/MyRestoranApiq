b2a97d0c58ad0c5438d50115e70dee867964872a
HomeLowchik <offnikita.nik0090@gmail.com>
2025-03-29 12:15:51 +0300

    dsad

--- Data/ReservationRequest.cs
+++ Data/ReservationRequest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace MyRestoranApi.Data
 {
@@ -43,7 +44,9 @@ namespace MyRestoranApi.Data
             Cancelled   // Отменено
         }
 
+        [JsonIgnore]
         public Customer Customer { get; set; }
+        [JsonIgnore]
         public Table Table { get; set; }
     }
 }
